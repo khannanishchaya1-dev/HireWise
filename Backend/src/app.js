004c247920ce147d3765a0bacf2654dd1ac2d868
Nishchaya <nish3654@gmail.com>
2026-06-12 20:32:36 +0530

    Final

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://hire-wise-alpha.vercel.app"
+      "https://hire-wise-omega.vercel.app"
     ],
     credentials: true,
   })
